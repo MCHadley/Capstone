a1309277c760784982b5cf87ca1dd51839ac5ebb
Michael <michael.c.hadley@gmail.com>
2019-02-27 20:13:04 -0500

    password hashed

--- registerUser.php
+++ registerUser.php
@@ -2,24 +2,28 @@
 include('dbClass.php');
 // New database connection
 $db = new Db();
+
 // user variables
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $userName = $_POST['userName'];
 $email = $_POST['email'];
 $pass = $_POST['password'];
+
+// hash password
+$options = ['cost' => 12, 'salt' => 'booksbooksbooksbooksbooksbooksbooksbooksbooksbooksbooksbooksbooks'];
+$passSecure = password_hash($pass, PASSWORD_BCRYPT, $options);
+
 // sanitize data
 $firstSan = $db->clean($firstName);
 $lastSan = $db->clean($lastName);
 $userSan = $db->clean($userName);
 $emailsSan = $db->clean($email);
-$passSan = $db->clean($pass);
+
 // insert data
 $queryIn = "INSERT INTO users(username, password, firstName, lastName, email)";
-$queryVa = "VALUES('".$userSan."','".$passSan."','".$firstSan."','".$lastSan."','".$emailsSan."')";
+$queryVa = "VALUES('".$userSan."','".$passSecure."','".$firstSan."','".$lastSan."','".$emailsSan."')";
 $queryFinal = $queryIn.$queryVa;
 $db->insert($queryFinal);
-
-
 
 ?>
