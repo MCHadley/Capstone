5e75b1bcd4cba28f49539ce1307bdb0b2d20c962
Michael <michael.c.hadley@gmail.com>
2019-03-14 15:12:43 -0400

    IF stmt for login, sani, and query

--- login.php
+++ login.php
@@ -2,14 +2,32 @@
 // Include database class and create new instance
 include('dbClass.php');
 $db = new Db();
+//Start session
+session_start();
+// Errors array
+$errors = array();
 
 //get variables from login page 
 $username = $_POST['username'];
 $pass = $_POST['password'];
 
-// create login query and run query
-$query = "SELECT username, password FROM users WHERE username ='".$username."'";
-$result = $db -> select($query);
+if(isset($_POST['logBtn'])){
+  //sanitize data
+  $userSan = $db->clean($username);
+  $passSan = $db->clean($pass);
+
+  if(empty($username)){array_push($errors, "Please enter a username");}
+  if(empty($pass)){array_pusy($errors, "Please enter a password");}
+  echo implode("<br>", $errors);
+
+  // create login query and run query
+  $query = "SELECT username, password FROM users WHERE username ='".$userSan."'";
+  $result = $db -> select($query);
+  $passCheck = $result['password'];
+
+}
+
+
 
 //If no user found error
 if(!$result){
