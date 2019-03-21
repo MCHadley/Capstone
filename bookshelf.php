8f50f4c481dcef83a58d8bfaff756985fac169d3
Michael <michael.c.hadley@gmail.com>
2019-03-21 17:05:27 -0400

    added statement for unregistered users

--- bookshelf.php
+++ bookshelf.php
@@ -2,7 +2,11 @@
 // Includes
 include('includes/header.php');
 include('includes/navbar.php');
-include('booklist.php');
+if(!$_SESSION['loggedin']){
+  echo "<p>Please login or create an account to view your booklist</p>";
+}else{
+  include('booklist.php');
+}
 include('includes/footer.php');
 
 ?>
