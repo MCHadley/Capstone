c871535d4a567b02131e465e47e7637e01f1e85c
Michael <michael.c.hadley@gmail.com>
2019-03-26 20:57:32 -0400

    moved search func

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include('includes/header.php');
 include('includes/navbar.php');
 include('includes/footer.php');
+include('includes/dbClass.php');
 ?>
 <div class="body-container">
   <div class="body-two">
@@ -10,7 +11,7 @@ include('includes/footer.php');
     <p>Books are added by users and you can add them to your lists!</p>
   </div>
   <div class="body-three">
-    <form method="POST" action="search.php">
+    <form method="POST" action="">
       <input type="text" id="searchBox" name="searchBox" class="form-control"
         placeholder="Search for a book by title or author!">
       <input type="submit" value="Search" name="submit" class="btn">
@@ -19,3 +20,35 @@ include('includes/footer.php');
   <div class="body-four">
   </div>
 </div>
+<?php
+if(isset($_POST['submit'])){
+  $db = new Db();
+  $connect = $db->connect();
+  $input = $_POST['searchBox'];
+  $querySan = $db->clean($input);
+  $formatQ = '%'.$querySan.'%';
+
+  $stmt = $connect->prepare("SELECT books.title, authors.authorFirst, authors.authorLast FROM books
+  INNER JOIN authors ON authors.author_id = books.author
+  WHERE books.title LIKE ? OR authors.authorFirst LIKE ? OR authors.authorLast LIKE ?");
+  $stmt->bind_param('sss', $formatQ, $formatQ, $formatQ);
+  $stmt->execute();
+  $stmt->store_result();
+  $stmt->bind_result($title, $authorFirst, $authorLast);
+  echo('<table><tr>
+          <th>Title</th>
+          <th>Author</th>
+        </tr>');
+  while($stmt->fetch()){
+    $authorName = $authorFirst." ".$authorLast;
+    printf("<tr>
+            <td>%s</td>
+            <td>%s</td>
+          </tr>", $title, $authorName);
+}
+echo('</table>');
+}else{
+  echo("<p>Please input a search query</p>");
+}
+
+?>
