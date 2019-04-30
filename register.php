4cea884d1f479564bb6ca0c7d646dbecb46af7ed
Michael <michael.c.hadley@gmail.com>
2019-04-30 14:08:19 -0400

    registration editing(label/css)

--- register.php
+++ register.php
@@ -9,15 +9,15 @@ include('includes/navbar.php');
     <div class="body-one">
     <h1>User Registeration</h1>
     <form method="post" action="registerScript.php" class="register">
-      <label for="firstName" hidden>First Name:</label>
+      <label for="firstName">First Name:</label>
       <input type="text" id="firstName" name="firstName" placeholder="First Name" class="form-control" required><br>
-      <label for="lastName" hidden>Last Name:</label>
+      <label for="lastName">Last Name:</label>
       <input type="text" id="lastName" name="lastName" placeholder="Last Name" class="form-control" required><br>
-      <label for="userName" hidden>Username:</label>
+      <label for="userName">Username:</label>
       <input type="text" id="userName" name="userName" placeholder="Username" class="form-control" required><br>
-      <label for="email" hidden>Email: </label>
+      <label for="email">Email: </label>
       <input type="email" id="email" name="email" placeholder="Email Address" class="form-control" required><br>
-      <label for="password" hidden>Password:</label> 
+      <label for="password">Password:</label> 
       <input type="password" id="password" name="password" placeholder="Password" class="form-control" required><br>
       <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
       <input type="submit" value="Register" name="regSubmit" class="btn">
